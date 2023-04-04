86877fa3ccd281b3f4d03e2bd260377c066db219
Josue <1323260@senati.pe>
2023-04-04 10:34:22 -0500

    Metodo listar terminaso en Curso

--- models/curso.php
+++ models/curso.php
@@ -6,18 +6,54 @@ require_once "./Conexion.php";
 class Curso extends Conexion{
 
   //objeto que almacene la conexion que viene desde el padre(conexion)
+  //y la compartira con todos los metodos (CRUD+)
   private $accesoBD;
 
 //constructor
-  public function _CONSTRUCT(){
-
+  public function __CONSTRUCT(){
+    $this->$accesoBD = parent::getConexion();
   }
 
   //metodo listar cursos
   public function listarCursos(){
-
+    try{
+      //1. preparamos la consulta
+      $consulta = $this->accesoBD->prepare("CALL spu_cursos_listar()");
+      //2.ejecutamos la consulta
+      $consulta->execute();
+      //3.devolvemos el resultado (array asociativo)
+      return $consulta->fetchAll(PDO::FETCH_ASSOC);
+ }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
   }
+
+  public function registrarCurso(){
+    try{
+
+    }
+    }catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+  public function eliminarCurso(){
+    try{
+
 }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+  public function actualizarCurso(){
+    try{
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+
   
 
 ?>
